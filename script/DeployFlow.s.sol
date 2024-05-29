455fae9b098d2cabd276b4089ff7fb8ada63787f
andreivladbrg <andreivladbrg@gmail.com>
2024-05-29 16:35:31 +0300

    refactor: update scripts with NFT Descriptor

--- script/DeployFlow.s.sol
+++ script/DeployFlow.s.sol
@@ -2,12 +2,18 @@
 pragma solidity >=0.8.22 <0.9.0;
 
 import { SablierFlow } from "src/SablierFlow.sol";
+import { SablierNFTDescriptor } from "src/SablierNFTDescriptor.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @notice Deploys {SablierFlow}.
 contract DeployFlow is BaseScript {
-    function run() public broadcast returns (SablierFlow flow) {
-        flow = new SablierFlow();
+    function run(address initialAdmin)
+        public
+        broadcast
+        returns (SablierFlow flow, SablierNFTDescriptor nftDescriptor)
+    {
+        nftDescriptor = new SablierNFTDescriptor();
+        flow = new SablierFlow(initialAdmin, nftDescriptor);
     }
 }
