57416f50cf0328607d2b2fc1a7822acaaebac86b
andreivladbrg <andreivladbrg@gmail.com>
2024-06-06 17:33:50 +0300

    test: add one more createAsset function
    test: rename Fixtures to Fuzz
    test: add a new create stream function in Integration_Test
    test: use bound over assume for decimals and rps
    test: add createStreamWithNewAsset in Fixtures contract
    test: group constants to increase readability
    test: rename function in utils to boundTransferAmount

--- test/integration/fuzz/Fuzz.t.sol
+++ test/integration/fuzz/Fuzz.t.sol
@@ -3,16 +3,9 @@ pragma solidity >=0.8.22;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { Base_Test } from "../../Base.t.sol";
 import { Integration_Test } from "../Integration.t.sol";
 
-abstract contract Shared_Integration_Fuzz_Test is Integration_Test {
-    /*//////////////////////////////////////////////////////////////////////////
-                                     VARIABLES
-    //////////////////////////////////////////////////////////////////////////*/
-
-    IERC20 internal asset;
-
+abstract contract Fuzz_Integration_Test is Integration_Test {
     /*//////////////////////////////////////////////////////////////////////////
                                      FIXTURES
     //////////////////////////////////////////////////////////////////////////*/
@@ -22,21 +15,27 @@ abstract contract Shared_Integration_Fuzz_Test is Integration_Test {
     uint256[19] public fixtureStreamId;
 
     /*//////////////////////////////////////////////////////////////////////////
-                                        SETUP
+                                        SET-UP
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public override {
-        Base_Test.setUp();
+        Integration_Test.setUp();
 
         _setupStreamsWithAllDecimals();
     }
 
     function _setupStreamsWithAllDecimals() private {
         for (uint8 decimal; decimal < 19; ++decimal) {
-            // Create asset, create stream and deposit.
-            IERC20 asset_ = createAsset(decimal);
+            uint256 nextStreamId = flow.nextStreamId();
 
-            uint256 streamId = createDefaultStreamWithAsset(asset_);
+            // Generate a random rate per second.
+            uint128 ratePerSecond =
+                uint128(_bound(uint256(keccak256(abi.encodePacked(nextStreamId, decimal))), 0.001e18, 10e18));
+
+            // Create asset, create stream and deposit.
+            IERC20 asset = createAsset(decimal);
+
+            uint256 streamId = createDefaultStreamWithRatePerSecondAndAsset(ratePerSecond, asset);
             depositDefaultAmountToStream(streamId);
 
             fixtureStreamId[decimal] = streamId;
