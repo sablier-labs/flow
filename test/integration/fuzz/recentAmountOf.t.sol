5713a6153a5388899519763b48a0bbfc8bdf32de
smol-ninja <shubhamy2015@gmail.com>
2024-06-15 21:25:23 +0100

    test(fuzz): recentAmountOf

---
+++ test/integration/fuzz/recentAmountOf.t.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+import { Shared_Integration_Fuzz_Test } from "./Fuzz.t.sol";
+
+contract RecentAmountOf_Integration_Fuzz_Test is Shared_Integration_Fuzz_Test {
+    /// @dev It should return 0.
+    ///
+    /// Given enough runs, all of the following scenarios should be fuzzed:
+    /// - Multiple paused streams, each with different asset decimals and rps.
+    /// - Multiple points in time.
+    function testFuzz_Paused(uint256 streamId, uint40 timeJump, uint8 decimals) external givenNotNull {
+        (streamId, decimals) = useFuzzedStreamOrCreate(streamId, decimals, true);
+
+        // Pause the stream.
+        flow.pause(streamId);
+
+        // Bound the time jump to provide a realistic time frame.
+        timeJump = boundUint40(timeJump, 1 seconds, 100 weeks);
+
+        // Simulate the passage of time.
+        vm.warp({ newTimestamp: getBlockTimestamp() + timeJump });
+
+        // Assert that amount owed is zero.
+        uint128 actualRecentAmount = flow.recentAmountOf(streamId);
+        assertEq(actualRecentAmount, 0, "recent amount");
+    }
+
+    /// @dev It should return 0.
+    ///
+    /// Given enough runs, all of the following scenarios should be fuzzed:
+    /// - Multiple non-paused streams, each with different asset decimals and rps.
+    function testFuzz_EqualLastTimeUpdate(
+        uint256 streamId,
+        uint40 timeJump,
+        uint8 decimals
+    )
+        external
+        givenNotNull
+        givenNotPaused
+    {
+        (streamId, decimals) = useFuzzedStreamOrCreate(streamId, decimals, true);
+
+        // Bound the time jump to provide a realistic time frame.
+        timeJump = boundUint40(timeJump, 1 seconds, 100 weeks);
+
+        // Simulate the passage of time.
+        vm.warp({ newTimestamp: getBlockTimestamp() + timeJump });
+
+        // Update the last time to block timestamp.
+        updateLastTimeToBlockTimestamp(streamId);
+
+        // Assert that amount owed is zero.
+        uint128 actualRecentAmount = flow.recentAmountOf(streamId);
+        assertEq(actualRecentAmount, 0, "recent amount");
+    }
+
+    /// @dev It should return the recent amount.
+    ///
+    /// Given enough runs, all of the following scenarios should be fuzzed:
+    /// - Multiple non-paused streams, each with different asset decimals and rps.
+    /// - Multiple points in time after the value of lastTimeUpdate.
+    function testFuzz_RecentAmountOf(
+        uint256 streamId,
+        uint40 timeJump,
+        uint8 decimals
+    )
+        external
+        givenNotNull
+        givenNotPaused
+    {
+        (streamId, decimals) = useFuzzedStreamOrCreate(streamId, decimals, true);
+
+        // Update the last time to block timestamp.
+        updateLastTimeToBlockTimestamp(streamId);
+
+        // Bound the time jump to provide a realistic time frame.
+        timeJump = boundUint40(timeJump, 1 seconds, 100 weeks);
+
+        // Simulate the passage of time.
+        vm.warp({ newTimestamp: getBlockTimestamp() + timeJump });
+
+        // Assert that amount owed is zero.
+        uint128 actualRecentAmount = flow.recentAmountOf(streamId);
+        uint128 expectedRecentAmount = flow.getRatePerSecond(streamId) * (getBlockTimestamp() - MAY_1_2024);
+        assertEq(actualRecentAmount, expectedRecentAmount, "recent amount");
+    }
+}
