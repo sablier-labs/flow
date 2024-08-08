8fec6bbc486085ab10422a4090a59237690d4eaf
Paul Razvan Berg <prberg@proton.me>
2024-08-08 13:50:27 +0300

    refactor: reorder imports

--- src/SablierFlow.sol
+++ src/SablierFlow.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.22;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Batch } from "./abstracts/Batch.sol";
