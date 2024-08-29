06a3b289ea36507413025a9252f63b6d0ccfaafd
andreivladbrg <andreivladbrg@gmail.com>
2024-08-29 15:31:58 +0300

    refactor: use UD21x18 for rate per second
    build: use github commit instead of NPM for PRBMath
    docs: update natspec
    test: update tests accordingly to use UD21x18 for rps

--- test/integration/fuzz/Fuzz.t.sol
+++ test/integration/fuzz/Fuzz.t.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.22;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ud21x18, UD21x18 } from "@prb/math/src/UD21x18.sol";
 
 import { Base_Test } from "../../Base.t.sol";
 import { Integration_Test } from "../Integration.t.sol";
