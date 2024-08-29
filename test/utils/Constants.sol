ca3bc8ddd10eff296bc161f97f3b6be95029327f
smol-ninja <shubhamy2015@gmail.com>
2024-08-29 16:36:25 +0100

    docs: polish

--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.22;
 
-import { ud21x18, UD21x18 } from "@prb/math/src/UD21x18.sol";
+import { UD21x18 } from "@prb/math/src/UD21x18.sol";
 import { UD60x18 } from "@prb/math/src/UD60x18.sol";
 
 abstract contract Constants {
