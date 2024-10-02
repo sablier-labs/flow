7af424d264fb7bf64d37c9573ba0e1861183d85b
andreivladbrg <andreivladbrg@gmail.com>
2024-10-03 00:58:35 +0300

    test: update block number forked

--- tests/fork/Fork.t.sol
+++ tests/fork/Fork.t.sol
@@ -37,9 +37,8 @@ abstract contract Fork_Test is Base_Test {
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual override {
-        // TODO: update the block number once 1st October 2024 is reached
         // Fork Ethereum Mainnet at a specific block number. The block number is for the `OCT_1_2024` date.
-        vm.createSelectFork({ blockNumber: 19_771_260, urlOrAlias: "mainnet" });
+        vm.createSelectFork({ blockNumber: 20_866_919, urlOrAlias: "mainnet" });
 
         // The base is set up after the fork is selected so that the base test contracts are deployed on the fork.
         Base_Test.setUp();
