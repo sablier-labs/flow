a6365c2c2efeaabc018d72aeb97c52819c4c4265
andreivladbrg <andreivladbrg@gmail.com>
2024-10-03 16:02:14 +0300

    refactor: rename nft descriptor contract
    refactor: change logo svg
    chore: update precompiles bytecode

--- script/GenerateSVG.s.sol
+++ script/GenerateSVG.s.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.8.22;
 import { IERC721Metadata } from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import { Script } from "forge-std/src/Script.sol";
 
-import { SablierFlowNFTDescriptor } from "src/SablierFlowNFTDescriptor.sol";
+import { FlowNFTDescriptor } from "src/FlowNFTDescriptor.sol";
 
 contract GenerateSVG is Script {
     function run() public returns (string memory svg) {
-        SablierFlowNFTDescriptor nftDescriptor = new SablierFlowNFTDescriptor();
+        FlowNFTDescriptor nftDescriptor = new FlowNFTDescriptor();
         svg = nftDescriptor.tokenURI(IERC721Metadata(address(0)), 0);
     }
 }
