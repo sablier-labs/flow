1119b342dd21ad3e9e37f7a9a1641c2afcbd97e7
smol-ninja <shubhamy2015@gmail.com>
2024-10-24 17:20:08 +0100

    build: rename test to tests in script

--- tests/integration/fuzz/withdrawMultiple.t.sol
+++ tests/integration/fuzz/withdrawMultiple.t.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.22;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ud21x18 } from "@prb/math/src/UD21x18.sol";
-import { ISablierFlow } from "src/interfaces/ISablierFlow.sol";
 
 import { Shared_Integration_Fuzz_Test } from "./Fuzz.t.sol";
 
