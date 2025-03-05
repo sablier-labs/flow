8de7aef6fb79fbd4d575f4ae25ea7f47eefb2a6e
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-03-05 17:19:59 +0200

    feat: add NFTDescriptor addresses

--- script/DeployFlow.s.sol
+++ script/DeployFlow.s.sol
@@ -4,14 +4,23 @@ pragma solidity >=0.8.22;
 import { BaseScript } from "@sablier/evm-utils/src/tests/BaseScript.sol";
 
 import { FlowNFTDescriptor } from "src/FlowNFTDescriptor.sol";
-import { FlowNFTDescriptor } from "src/FlowNFTDescriptor.sol";
 import { SablierFlow } from "src/SablierFlow.sol";
 
+import { NFTDescriptorAddresses } from "./NFTDescriptorAddresses.sol";
+
 /// @notice Deploys {SablierFlow}.
-contract DeployFlow is BaseScript {
+contract DeployFlow is BaseScript, NFTDescriptorAddresses {
     function run() public broadcast returns (SablierFlow flow, FlowNFTDescriptor nftDescriptor) {
         address initialAdmin = protocolAdmin();
-        nftDescriptor = new FlowNFTDescriptor();
+        // If the contract is not deployed, deploy it.
+        if (nftDescriptorAddress() == address(0)) {
+            nftDescriptor = new FlowNFTDescriptor();
+        }
+        // Otherwise, use the address of the existing contract.
+        else {
+            nftDescriptor = FlowNFTDescriptor(nftDescriptorAddress());
+        }
+
         flow = new SablierFlow(initialAdmin, nftDescriptor);
     }
 }
